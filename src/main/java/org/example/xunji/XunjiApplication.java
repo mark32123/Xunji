0511cb59550c2a34df721f92ea4ec733858a5c20
mark <2483178594@qq.com>
2025-09-22 19:45:43 +0800

    Initial commit

---
+++ src/main/java/org/example/xunji/XunjiApplication.java
@@ -0,0 +1,13 @@
+package org.example.xunji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XunjiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XunjiApplication.class, args);
+    }
+
+}
